d6d1e080ba03a914dd367bfc72d004a16d7b6d0f
Tschipcraft <78470530+Tschipcraft@users.noreply.github.com>
2023-01-18 17:19:43 +0100

    Better placement for bubble particles when opening chests underwater

--- src/main/java/net/tschipcraft/make_bubbles_pop/event/BlockInteractHandler.java
+++ src/main/java/net/tschipcraft/make_bubbles_pop/event/BlockInteractHandler.java
@@ -1,9 +1,12 @@
 package net.tschipcraft.make_bubbles_pop.event;
 
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.block.BarrelBlock;
 import net.minecraft.block.ChestBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.tag.FluidTags;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
@@ -17,12 +20,24 @@ public class BlockInteractHandler implements UseBlockCallback {
     public ActionResult interact(PlayerEntity player, World world, Hand hand, BlockHitResult hitResult) {
         BlockPos blockPos = hitResult.getBlockPos();
 
-        if (hitResult.getType() == HitResult.Type.BLOCK && !player.isSneaking() && !player.isSpectator()) {
+        if (hitResult.getType() == HitResult.Type.BLOCK && !player.isSpectator() && !player.isSneaking()) {
 
             if (world.getBlockState(blockPos).getBlock() instanceof ChestBlock) {
                 if (!ChestBlock.isChestBlocked(world, blockPos) && world.getFluidState(blockPos).isIn(FluidTags.WATER)) {
                     for (int i = (int)(Math.random() * 5) + 5; i >= 0; i--) {
-                        world.addParticle(ParticleTypes.BUBBLE, blockPos.getX() + Math.random(), blockPos.getY() + 0.7 - (Math.random()/3.0), blockPos.getZ() + Math.random(), 0f, 0.1f, 0f);
+                        world.addParticle(ParticleTypes.BUBBLE, blockPos.getX() + 0.5 + ((0.5 - Math.random())/2.0), blockPos.getY() + 0.7 - (Math.random()/2.0), blockPos.getZ() + 0.5 + ((0.5 - Math.random())/2.0), 0f, 0.1f, 0f);
+                        world.playSound(blockPos.getX(), blockPos.getY(), blockPos.getZ(), SoundEvents.BLOCK_BUBBLE_COLUMN_WHIRLPOOL_AMBIENT, SoundCategory.AMBIENT, 0.1f, 1.4f, false);
+                    }
+
+                }
+            }
+
+            if (world.getBlockState(blockPos).getBlock() instanceof BarrelBlock) {
+
+                if (world.getFluidState(blockPos.up()).isIn(FluidTags.WATER)) {
+                    for (int i = (int)(Math.random() * 5) + 5; i >= 0; i--) {
+                        world.addParticle(ParticleTypes.BUBBLE, blockPos.getX() + 1.0 + ((0.5 - Math.random())/2.0), blockPos.getY() + 0.7 - (Math.random()/2.0), blockPos.getZ() + 0.5 + ((0.5 - Math.random())/2.0), 0f, 0.1f, 0f);
+                        world.playSound(blockPos.getX(), blockPos.getY(), blockPos.getZ(), SoundEvents.BLOCK_BUBBLE_COLUMN_WHIRLPOOL_AMBIENT, SoundCategory.AMBIENT, 0.1f, 1.4f, false);
                     }
 
                 }
