0e6441f004f5dcea89ea99a4cfddac532de707ba
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-02-24 14:36:55 -0500

    Remove Unneeded Packet

--- fabric/src/main/java/net/tschipcraft/make_bubbles_pop/MakeBubblesPop.java
+++ fabric/src/main/java/net/tschipcraft/make_bubbles_pop/MakeBubblesPop.java
@@ -2,15 +2,12 @@ package net.tschipcraft.make_bubbles_pop;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MakeBubblesPop implements ModInitializer {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger("make_bubbles_pop");
-
-	public static final Identifier BARREL_BUBBLE_PACKET = new Identifier("make_bubbles_pop", "barrel_bubble_packet");
 
 	public static final boolean MIDNIGHTLIB_INSTALLED = FabricLoader.getInstance().isModLoaded("midnightlib");
 
--- fabric/src/main/java/net/tschipcraft/make_bubbles_pop/MakeBubblesPopClient.java
+++ fabric/src/main/java/net/tschipcraft/make_bubbles_pop/MakeBubblesPopClient.java
@@ -1,29 +1,12 @@
 package net.tschipcraft.make_bubbles_pop;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.tschipcraft.make_bubbles_pop.impl.BarrelBubbler;
 
 public class MakeBubblesPopClient implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
 		MakeBubblesPop.LOGGER.info("Make Bubbles Pop by Tschipcraft initialized! (Client entrypoint)");
-		receiveBarrelBubblePacket();
-	}
-
-	private static void receiveBarrelBubblePacket() {
-		ClientPlayNetworking.registerGlobalReceiver(MakeBubblesPop.BARREL_BUBBLE_PACKET, (ctx, handler, byteBuf, responseSender) -> {
-			BlockPos pos = new BlockPos(byteBuf.readInt(), byteBuf.readInt(), byteBuf.readInt());
-			Direction facing = Direction.byName(byteBuf.readString());
-			ctx.execute(() -> {
-				if (ctx.world == null)
-					throw new IllegalStateException("Your world is null? How?");
-				BarrelBubbler.spawnBubbles(ctx.world, pos, facing, ctx.world.random);
-			});
-		});
 	}
 
 }
